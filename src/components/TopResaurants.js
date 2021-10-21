e7ea9f84d47e9b209edbac76af480bafabcb8e42
h.talha <h.talha@al-mouradia.dz>
2021-10-21 15:59:46 +0100

    edit css

--- src/components/TopResaurants.js
+++ src/components/TopResaurants.js
@@ -3,6 +3,7 @@ import '../css/top-restaurants.css'
 import rest_img1 from '../imgs/restau1.png'
 import rest_img2 from '../imgs/restau2.png'
 import see_more_img from '../imgs/see_more.png'
+import pin from '../imgs/location_.png'
 const TopResaurants = () => {
     return (
         <section>
@@ -18,8 +19,22 @@ const TopResaurants = () => {
             </div>
         </div>
         <div className="restau-grid d-flex align-items-center">
-            <div className="restaurant">
+            <div className="restaurant" >
                 <img src={rest_img2} width="100%" alt="restau"/>
+                <div className="infos" >
+                    <h4 className="mb-2">Fire Water</h4>
+                    <div class="sub" style={{maxWidth:'60%'}}>
+                        we are all about we are all about to the fullest
+                        and all content dining out or in!dining out or in!
+                    </div>
+                    <div className="d-flex align-items-center location">
+                        <img src={pin} alt="location" width="20" height="25"/>
+                        <span className="ml-4">New Market</span>
+                    </div>
+                    <div className="book-btn bg-white text-center primary">
+                    Book Now
+                    </div>
+                </div>
             </div>
             <div className="restaurant">
                 <img src={rest_img1} width="100%"  alt="restau"/>
